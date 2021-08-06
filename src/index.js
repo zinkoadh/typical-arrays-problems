39e254bf78f78fb97e1554ec02863d2395f5ab89
zinkoadh <pavel.karabinovich@gmail.com>
2021-08-06 20:05:53 +0300

    feat: update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 
 exports.min = function min (array) {
-  return 0;
+  return (typeof array ==="undefined" || array.length === 0)?0: array.sort((a,b) => a-b)[0];
 }
 
 exports.max = function max (array) {
-  return 0;
+  return (typeof array ==="undefined"|| array.length === 0)?0: array.sort((a,b) => b-a)[0];
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  return (typeof array ==="undefined"|| array.length === 0)?0: array.reduce((a,b) => a+b)/array.length;
 }
